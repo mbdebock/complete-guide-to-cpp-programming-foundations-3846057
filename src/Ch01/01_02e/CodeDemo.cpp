76d06a5d17d4a1fa82d1f9b44897b5b64423d3b9
mbdebock <102904816+mbdebock@users.noreply.github.com>
2025-04-03 20:50:35 +0000

    difference between argc and argv

--- src/Ch01/01_02e/CodeDemo.cpp
+++ src/Ch01/01_02e/CodeDemo.cpp
@@ -3,9 +3,23 @@
 // Hello World, by Eduardo Corpeño 
 
 #include <iostream>
+#include <cstdio>
 
-int main(){
+/* This program prints
+   the message "Hi There!"
+   in the terminal output. */
+
+/*  argc = Integer that represents the number of command line arguments passed to the program.
+    argv = an array of C-style strings representing those arguments.
+int main(int argc, char* argv[]){
     std::cout << "Hi There!" << std::endl;
+    return 0;
+}
+*/
+
+int main(){ //This is the main function
+    std::cout << "Hi There!" << std::endl;
+    printf("Hey, there!\n");
 
     std::cout << std::endl << std::endl;
     return 0;
